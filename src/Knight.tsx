1ed270c2d23deae46728de0005ef60f019b08cdc
hiromoon <hiromoon0428@gmail.com>
2020-05-09 04:35:31 +0900

    dragのhooksを呼び出した

--- src/Knight.tsx
+++ src/Knight.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { ItemTypes } from './Constants'
+import { useDrag }  from 'react-dnd'
 
 export const Knight = () => {
-  return <span>♘</span>
+  const [{isDragging}, drag] = useDrag({
+    item: { type: ItemTypes.KNIGHT },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    })
+  })
+
+  return (
+    <div 
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+          fontWeight: 'bold',
+          cursor: 'move',
+      }}
+    >
+      ♘
+    </div>
+  )
 }
 
